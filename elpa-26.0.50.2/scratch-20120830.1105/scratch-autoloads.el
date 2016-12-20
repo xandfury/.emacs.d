59835c503d8012813c24aee2d86782181f54967e
Abhinav Saxena <xandfury@gmail.com>
2016-12-20 12:30:16 +0530

    minor fix in startup ; company

---
+++ elpa-26.0.50.2/scratch-20120830.1105/scratch-autoloads.el
@@ -0,0 +1,23 @@
+;;; scratch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "scratch" "scratch.el" (0 0 0 0))
+;;; Generated autoloads from scratch.el
+
+(autoload 'scratch "scratch" "\
+Get a scratch buffer for the current mode.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "scratch" '("scratch-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; scratch-autoloads.el ends here
